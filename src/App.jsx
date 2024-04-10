2f8611bee99a9907e6d3007971aec419875883ab
Mahdi <mahdimey17@gmail.com>
2024-04-10 14:39:39 +0330

    seperated Input & Button to their own re-usable components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import styled from "styled-components"
+import GlobalStyles from "./styles/GlobalStyles"
+import Button from "./ui/Button"
+import Input from "./ui/Input"
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`
+
 function App() {
-  return <>hello world</>
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Button>Check In</Button>
+        <Input type="number" placeholder="Number of guests" />
+      </StyledApp>
+    </>
+  )
 }
 
 export default App
