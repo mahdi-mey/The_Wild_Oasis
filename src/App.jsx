ea939ba4af14ce7e02c7a2ac0490f7db5e8aa3be
Mahdi <mahdimey17@gmail.com>
2024-04-11 13:51:38 +0330

    added routes to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,40 @@
-import styled from "styled-components"
+// import styled from "styled-components"
 import GlobalStyles from "./styles/GlobalStyles"
-import Button from "./ui/Button"
-import Input from "./ui/Input"
-import Heading from "./ui/Heading"
+// import Button from "./ui/Button"
+// import Input from "./ui/Input"
+// import Heading from "./ui/Heading"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import Dashboard from "./pages/Dashboard"
+import Cabins from "./pages/Cabins"
+import Users from "./pages/Users"
+import Settings from "./pages/Settings"
+import Account from "./pages/Account"
+import Login from "./pages/Login"
+import PageNotFound from "./pages/PageNotFound"
 
-const StyledApp = styled.div`
-  background-color: orangered;
-  padding: 20px;
-`
+// const StyledApp = styled.div`
+//   background-color: orangered;
+//   padding: 20px;
+// `
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <StyledApp>
-        <Heading>The Wild Oasis</Heading>
-        <Button size="small" variation="secondary">
-          Check In
-        </Button>
-        <Input type="number" placeholder="Number of guests" />
-      </StyledApp>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to="dashboard" />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="cabins" element={<Cabins />} />
+          <Route path="users" element={<Users />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="account" element={<Account />} />
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+      {/* <StyledApp> */}
+      {/* </StyledApp> */}
     </>
   )
 }
