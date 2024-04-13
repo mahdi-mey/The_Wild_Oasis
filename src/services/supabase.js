c5c7d19ce4d3a00b4b416560e3b6cc6fba24341c
Mahdi <mahdimey17@gmail.com>
2024-04-13 20:03:00 +0330

    read the value that comes from the backend

---
+++ src/services/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://hwqhrpgyjhtyxnsrxrow.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh3cWhycGd5amh0eXhuc3J4cm93Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI4NjMwNDUsImV4cCI6MjAyODQzOTA0NX0.OhTjR6HK-bSrgTqp30nOZez2T-77Ko4shtTmriwANmk'
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default supabase
