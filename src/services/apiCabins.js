dfd03c6c4dc20a438e0160b4214ff57931f55b62
Mahdi <mahdimey17@gmail.com>
2024-04-18 12:01:17 +0330

    implemented delettinh cabin functionality

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -11,3 +11,18 @@ export async function getCabins() {
     console.log(data)
     return data
 }
+
+export async function deleteCabin(id) {
+
+    const { data, error } = await supabase
+        .from('cabins')
+        .delete()
+        .eq('id', id)
+
+    if (error) {
+        console.log(error)
+        console.log('cabin could not be deleted')
+    }
+    console.log(data)
+    return data
+}
