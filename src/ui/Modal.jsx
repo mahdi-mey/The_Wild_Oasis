1acc59bd842d6051f7a8777838595833f3bc4589
Mahdi <mahdimey17@gmail.com>
2024-05-27 22:10:33 +0330

    add closing modal by clicking outside of component logic

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components"
 import { createPortal } from "react-dom"
-import { cloneElement, createContext, useContext, useState } from "react"
+import { cloneElement, createContext, useContext, useEffect, useRef, useState } from "react"
 
 const StyledModal = styled.div`
   position: fixed;
@@ -73,12 +73,26 @@ function Open({ children, opens: opensWindowName }) {
 
 function Window({ children, name }) {
   const { openName, close } = useContext(ModalContext)
+  const ref = useRef()
+
+  useEffect(() => {
+    function handleClick(e) {
+      if (ref.current && !ref.current.contains(e.target)) {
+        console.log('clicked')
+        close()
+      }
+    }
+
+    document.addEventListener('click', handleClick, true)
+
+    return () => document.removeEventListener('click', handleClick, true)
+  }, [close])
 
   if (name !== openName) return null
-
+  
   return createPortal(
     <Overlay>
-      <StyledModal>
+      <StyledModal ref={ref}>
         <Button onClick={close}>❌</Button>
         <div>
           {cloneElement(children, {
