3a374aed0c5a96bf56f84f5a0dc7e638f27c3ba7
Mahdi <mahdimey17@gmail.com>
2024-06-04 10:28:53 +0330

    add sorting feature and save them in url

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -29,18 +29,29 @@ function CabinTable() {
   if (isLoading) return <Spinner />
 
   const filterValue = searchParams.get("discount") || "all"
-  
-  let filteredCabins;
 
-  if(filterValue === "all") filteredCabins = cabins
-  if(filterValue === 'no-discount') filteredCabins = cabins.filter((cabin) => cabin.discount === 0)
-  if(filterValue === 'with-discount') filteredCabins = cabins.filter((cabin) => cabin.discount > 0)
+  let filteredCabins
+
+  // 1) FILTER
+  if (filterValue === "all") filteredCabins = cabins
+  if (filterValue === "no-discount")
+    filteredCabins = cabins.filter((cabin) => cabin.discount === 0)
+  if (filterValue === "with-discount")
+    filteredCabins = cabins.filter((cabin) => cabin.discount > 0)
+
+  // 2) SORT
+  const sortBy = searchParams.get("sortBy") || "startDate-asc"
+  const [field, direction] = sortBy.split("-")
+  const modifier = direction === "asc" ? 1 : -1
+  const sortedCabins = filteredCabins.sort(
+    (a, b) => (a[field] - b[field]) * modifier
+  )
 
   return (
     <Menus>
       <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
         <Table.Header>
-          <div></div> 
+          <div></div>
           <div>Cabin</div>
           <div>Capacity</div>
           <div>Price</div>
@@ -49,7 +60,7 @@ function CabinTable() {
         </Table.Header>
 
         <Table.Body
-          data={filteredCabins}
+          data={sortedCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
